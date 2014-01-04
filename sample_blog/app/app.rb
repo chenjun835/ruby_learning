9234c395ec414a19d5bb17dd3537d5914bb8cef4
Jun Chen <jun.chen@mixi.co.jp>
2014-01-04 18:31:27 +0900

    Implement blog.

--- sample_blog/app/app.rb
+++ sample_blog/app/app.rb
@@ -8,6 +8,14 @@ module SampleBlog
 
     enable :sessions
 
+    get "/" do
+        "Hello World!"
+    end
+
+    get :about, :map => '/about_us' do
+        render :haml,"%p This is a sample blog created to demonstrate how Padrino works!"
+    end
+
     ##
     # Caching support.
     #
